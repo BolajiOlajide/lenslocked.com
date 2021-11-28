cc01f25e93e1b5d5d1e771cafadfc199dcadd5aa
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2021-11-28 14:54:31 +0100

    feat: use modd for dynamic reloading

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/BolajiOlajide/lenslocked.com
 
-go 1.15
+go 1.17
--- main.go
+++ main.go
@@ -6,10 +6,12 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
 func main() {
 	http.HandleFunc("/", handlerFunc)
+	fmt.Println("Starting the server on port 4000")
 	http.ListenAndServe(":4000", nil)
 }
