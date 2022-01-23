7d3a8abdca3b5bc51ec426d9581376dfb454911a
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2022-01-23 17:53:18 +0100

    feat: setup pg connection with pg

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,67 +1,22 @@
 package main
 
 import (
-	"errors"
+	"database/sql"
 	"fmt"
-	"html/template"
-	"os"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
-// User structure for a user
-type User struct {
-	Name string
-	Age  int
-	// anonymous struct
-	// Meta struct {
-	// 	Visits int
-	// }
-	Meta    UserMeta
-	Bio     string
-	RawHTML template.HTML
-}
-
-// UserMeta giving meta info about a user
-type UserMeta struct {
-	Visits int
-}
-
-// ErrNotFound signifies when an item isn't found
-var ErrNotFound = errors.New("not found")
-
 func main() {
-	t, err := template.ParseFiles("cmd/exp/hello.gohtml")
-
+	db, err := sql.Open("pgx", "host=localhost port=5432 user=baloo password=junglebook dbname=lenslocked sslmode=disable")
 	if err != nil {
 		panic(err)
 	}
+	defer db.Close()
 
-	user := User{
-		Name: "Susan Smith",
-		Age:  111,
-		Meta: UserMeta{
-			Visits: 4,
-		},
-		Bio:     `<script>alert("I am a user")</script>`,
-		RawHTML: `<script>console.log("I AM A SCRIPT")</script>`,
-	}
-
-	err = t.Execute(os.Stdout, user)
+	err = db.Ping()
 	if err != nil {
 		panic(err)
 	}
-
-	err = b()
-	fmt.Println(errors.Is(err, ErrNotFound))
-	unwrappedError := errors.Unwrap(err)
-	fmt.Println(unwrappedError)
-	// TODO determine if err is an ErrNotFound
-}
-
-func a() error {
-	return ErrNotFound
-}
-
-func b() error {
-	err := a()
-	return fmt.Errorf("b: %v", err)
+	fmt.Println("connected")
 }
